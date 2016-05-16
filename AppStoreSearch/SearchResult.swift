7f665062636f86006624eb2edafab262113b3989
yyang005 <yingyang@ying-yangs-macbook-pro.local>
2016-05-15 22:23:02 -0400

    Add delegate methods for searcher and tableview. Add data model.

---
+++ AppStoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  AppStoreSearch
+//
+//  Created by ying yang on 5/15/16.
+//  Copyright © 2016 ying yang. All rights reserved.
+//
+
+import Foundation
+
+class searchResult {
+    var name: String = ""
+    var artistName: String = ""
+}
--- AppStoreSearch/SearchViewController.swift
+++ AppStoreSearch/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource {
 
     // MARK: IBOutlets
     
@@ -23,7 +23,26 @@ class SearchViewController: UIViewController {
         
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
-
-
+    
+    // MARK: search bar delegate
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
+    }
+    
+    // MARK: table view data source methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "searchResult")
+        return cell
+    }
 }
 
