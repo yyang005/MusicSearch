d3e818fe68601349251f0620baef13a2185a551f
yyang005 <yingyang@ying-yangs-macbook-pro.local>
2016-05-27 12:44:54 -0400

    Add customize controlls to popup view

--- AppStoreSearch/DetailViewController.swift
+++ AppStoreSearch/DetailViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // MARK: IBOutlets and IBAction
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    
     @IBAction func close(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func close() {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
@@ -19,10 +32,27 @@ class DetailViewController: UIViewController {
         modalPresentationStyle = .Custom
         transitioningDelegate = self
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "close")
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
+    }
 }
 
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimPresentationController( presentedViewController: presented, presentingViewController: presenting)
+    }
+}
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        return touch.view === self.view
     }
 }
