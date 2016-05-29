377e66801080c44fb8337af458399b1b7ff6c699
yyang005 <yingyang@ying-yangs-macbook-pro.local>
2016-05-29 17:53:42 -0400

    Added scroll view and page control

--- AppStoreSearch/LandscapeViewController.swift
+++ AppStoreSearch/LandscapeViewController.swift
@@ -10,4 +10,34 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    // MARK: life cycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(view.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+
+        pageControl.removeConstraints(view.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        pageControl.frame = CGRect(x: 0, y: view.bounds.height - pageControl.frame.height,
+            width: view.bounds.width,
+            height: pageControl.frame.height)
+    }
 }
